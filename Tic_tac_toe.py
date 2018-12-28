0ccd822a19849a8c1a43743b10247673b003d255
Unknown <santiago.espinosa14@pm.me>
2018-12-28 09:49:08 +0800

    Updated comments and removed unused variable

--- Tic_tac_toe.py
+++ Tic_tac_toe.py
@@ -7,7 +7,7 @@ def placer(plane, choice, side):		#function to place user's choice on the field
     plane[splitSide1][splitSide2] = sider
     return plane
 
-def printer(plane):		#function to print out the current board
+def printer(plane):		                    #function to print out the current board
     for w in range(len(plane)):
         for e in range(len(plane)):
             if plane[w][e]== 1:
@@ -18,7 +18,7 @@ def printer(plane):		#function to print out the current board
                 print('{:>3d}'.format((len(plane)*w+e)), end="")
         print("")
 
-def listSum(numList):		#Function to add elements of a list together
+def listSum(numList):		            #Function to add elements of a list together
    if len(numList) == 1:
         return numList[0]
    else:
@@ -27,14 +27,13 @@ def listSum(numList):		#Function to add elements of a list together
 def winnerFind(plane, side):		#check for a winner in 3 different ways:
     
 	#check for row win by adding values to see if they're equal to the length of the playing plane
-    win=False
     tempVar=[]
     for g in range(len(plane)):
         if listSum(plane[g]) == len(plane) or listSum(plane[g]) == -int(len(plane)):
             print("Winner:", side)
             exit()
        
-    #cheak for column win by adding values:
+    #check for column win by adding values:
     for t in range(len(plane)):
         for u in range(len(plane)):
             tempVar.append(plane[u][t])
@@ -81,29 +80,30 @@ def endOfGame(plane):		#check whether all positions are taken up
         exit()
 
 
-def main():		#Choose active side and call corresponding functions
+def main():		#Main function selects active side (always X) and calls corresponding functions
     side="X"
     print("Size -->", end="")
     size=int(input())
     plane=[]
-    for g in range(size):
+    for g in range(size):               #create initial (empty) 2-Dimensional array with user-chosen size
         plane.append([0]*size)
     printer(plane)
     
-    while True:
+    while True: 
         if side=="X":
             print("X--> ", end="")
         if side=="O":
             print("O--> ", end="")
 			
         choice=int(input())
-        choice=choiceTrans(choice, plane)
-        plane=placer(plane, choice, side)
-        printer(plane)
-        winnerFind(plane, side)
-        if side == "X":
+        choice=choiceTrans(choice, plane)       #choice "translates" user input into computeable values
+        plane=placer(plane, choice, side)         #Places user's choice on the array with a 1 for X and a -1 for O
+        printer(plane)                                        #Prints (now modified) array to screen, replacing empty locations with a corresponding number  
+        winnerFind(plane, side)                         #Finds the winner based on whether the absolute value of the sum of a line is equal to the length of the array
+        
+        if side == "X":     
             side="O"
         elif side=="O":
             side="X"
-        endOfGame(plane)
+        endOfGame(plane)                                #Checks whether the field is full. Only executes after winnerFind() in order to make sure when the field is full, no one has won yet
 main()
